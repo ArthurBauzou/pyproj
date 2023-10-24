75e1ff40e37ffd7eb134a54dc5f76a11ee289737
Arthur BAUZOU <arthurbauzou@gmail.com>
2023-10-24 11:28:40 +0200

    flasque get users

--- flasque/app.py
+++ flasque/app.py
@@ -3,11 +3,15 @@ from flask import render_template
 from pymongo import MongoClient, errors
 
 app = Flask(__name__)
+app.testing = True
+app.config.update(
+    TEMPLATES_AUTO_RELOAD = True
+)
 
 #––– SGBD –––#
 client = MongoClient("mongodb://localhost:27017/")
 db = client.colorDB
-users = db.users
+# total_users = db.users.find()
 
 user_props = {
     'username': '',
@@ -20,4 +24,11 @@ user_props = {
 def home():
     return render_template('home.html',
         user = user_props.keys()
+    )
+
+@app.route('/users')
+def get_users():
+    total_users = db.users.find()
+    return render_template('users.html',
+                           users = total_users
     )
